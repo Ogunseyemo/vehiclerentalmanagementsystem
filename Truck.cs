3267b3acca2e3483c836557415bbb092ee6c6b25
Ogunseyemo <ogunseyemt@gmail.com>
2024-03-27 18:12:15 +0100

    Truck class

--- Truck.cs
+++ Truck.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace vehiclerentalmanagementsystem
 {
-    internal class Truck
+    using System;
+
+    public class Truck : Vehicle
     {
+        // Additional properties for trucks
+        public double Capacity { get; set; }
+        public string TruckType { get; set; }
+        public bool FourWheelDrive { get; set; }
+
+        // Constructor
+        public Truck(string model, string manufacturer, int year, double rentalPrice, double capacity, string truckType, bool fourWheelDrive) : base(model, manufacturer, year, rentalPrice)
+        {
+            Capacity = capacity;
+            TruckType = truckType;
+            FourWheelDrive = fourWheelDrive;
+        }
+
+        // Override method to display truck details
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine($"Capacity: {Capacity} tons\nTruck Type: {TruckType}\nFour-Wheel Drive: {FourWheelDrive}");
+        }
     }
 }
