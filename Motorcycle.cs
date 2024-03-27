e6cd0492b4c992c8e7fcc5a28c057667f021b771
Ogunseyemo <ogunseyemt@gmail.com>
2024-03-27 20:26:56 +0100

    motorcycle class

--- Motorcycle.cs
+++ Motorcycle.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace vehiclerentalmanagementsystem
 {
-    internal class Motorcycle
+
+    public class Motorcycle : Vehicle
     {
+        // Additional properties for motorcycles
+        public int EngineCapacity { get; set; }
+        public string FuelType { get; set; }
+        public bool HasFairing { get; set; }
+
+        // Constructor
+        public Motorcycle(string model, string manufacturer, int year, double rentalPrice, int engineCapacity, string fuelType, bool hasFairing) : base(model, manufacturer, year, rentalPrice)
+        {
+            EngineCapacity = engineCapacity;
+            FuelType = fuelType;
+            HasFairing = hasFairing;
+        }
+
+        // Override method to display motorcycle details
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine($"Engine Capacity: {EngineCapacity} cc\nFuel Type: {FuelType}\nHas Fairing: {HasFairing}");
+        }
     }
 }
